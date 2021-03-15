cbf3ab17fca16f650d086018eef6104dc3a5a4a1
jnlon <jnlon@users.noreply.github.com>
2021-03-15 13:43:29 -0400

    Refactor and cleanup various GameEntities

--- src/MarbleSorterGame/MarbleSorterGame/Bucket.cs
+++ src/MarbleSorterGame/MarbleSorterGame/Bucket.cs
@@ -8,22 +8,19 @@ namespace MarbleSorterGame
     public class Bucket : GameEntity
     {
         private Sprite _bucket;
-        private Sound _dropSound;
+        //private Sound _dropSound;
         private Sound _failSound;
         private Sound _successSound;
         private Color _requiredColor;
         private Weight _requiredWeight;
 
+        private Text _capacityLabel;
+        private RectangleShape _requiredColorLabel;
+
         public int Capacity;
         public int Accepted;
-        public bool Fail { get; }
 
-        /// <summary>
         /// Bucket that holds dropped marbles, containing requirements for color, weight and capacity
-        /// </summary>
-        /// <param name="requiredColor"></param>
-        /// <param name="requiredWeight"></param>
-        /// <param name="capacity"></param>
         public Bucket(Vector2f position, Vector2f size, Color requiredColor, Weight requiredWeight, int capacity) :  base (position, size)
         {
             _requiredColor = requiredColor;
@@ -31,58 +28,72 @@ namespace MarbleSorterGame
             Capacity = capacity;
 
             _bucket = new Sprite();
-            _bucket.Position = Position;
+
+            Vector2f bucketSize = requiredWeight switch
+            {
+                Weight.Large => new Vector2f(250, 250),
+                Weight.Medium => new Vector2f(200, 200),
+                Weight.Small => new Vector2f(150, 150)
+            };
+            
+            Size = bucketSize;
+            _bucket.Position = Position - new Vector2f(0, Size.Y);
+            
+            _requiredColorLabel = new RectangleShape()
+            {
+                FillColor = requiredColor.ToSfmlColor(),
+                Position = new Vector2f(Position.X + Size.X/2, Position.Y - Size.Y/2),
+                Size = new Vector2f(40, 40)
+            };
+            
+            _requiredColorLabel.Origin = new Vector2f(_requiredColorLabel.Size.X / 2, _requiredColorLabel.Size.Y / 2);
         }
 
-        /// <summary>
-        /// Plays sound of marble dropping into the bucket
-        /// </summary>
-        public void PlayDropSound()
+        /// Insert marble into the bucket, return true/false depening on whether marble meets its requirements
+        public bool InsertMarble(Marble m)
         {
-            _dropSound.Play();
+            if (m.Color == _requiredColor && m.Weight == _requiredWeight && Accepted < Capacity)
+            {
+                Accepted++;
+                _successSound.Play();
+                _capacityLabel.DisplayedString = $"{Accepted}/{Capacity}";
+                return true;
+            }
+            else
+            {
+                _capacityLabel.DisplayedString = $"{Accepted}/{Capacity}";
+                _failSound.Play();
+                return false;
+            }
         }
 
-        /// <summary>
-        /// Plays sound that indicates marble correctly fits all of the bucket requirements
-        /// </summary>
-        public void PlaySuccessSound()
-        {
-            _successSound.Play();
-        }
-
-        /// <summary>
-        /// Plays sound that indicates marble did not fit bucket requirements
-        /// </summary>
-        public void PlayFailSound()
-        {
-            _failSound.Play();
-        }
-
-        /// <summary>
         /// Draws the bucket onto render target RenderWindow
-        /// </summary>
-        /// <param name="window">RenderWindow for marble to be drawn onto</param>
         public override void Render(RenderWindow window)
         {
+           // base.Render(window);
             if (_bucket == null)
                 return;
-
+            
             window.Draw(_bucket);
+            window.Draw(_requiredColorLabel);
+            window.Draw(_capacityLabel);
         }
-
-        /// <summary>
+        
         /// Extracts bucket assets, such as texture and sound, from bundle
-        /// </summary>
-        /// <param name="bundle"></param>
         public override void Load(IAssetBundle bundle)
         {
-            _dropSound = bundle.BucketDrop;
+            _capacityLabel = new Text("", bundle.Font);
+            // TODO: Find a better place for this
+            _capacityLabel.Position = Position + new Vector2f(Size.X/2, 10);
+            _capacityLabel.DisplayedString = $"0/{Capacity}";
+            _capacityLabel.FillColor = SFML.Graphics.Color.Black;
+            
+            // _dropSound = bundle.BucketDrop;
             _successSound = bundle.BucketDropSuccess;
             _failSound = bundle.BucketDropFail;
 
             _bucket.Texture = bundle.BucketTexture;
             _bucket.Scale = ScaleEntity(bundle.BucketTexture);
         }
-
     }
 }
--- src/MarbleSorterGame/MarbleSorterGame/Gate.cs
+++ src/MarbleSorterGame/MarbleSorterGame/Gate.cs
@@ -6,96 +6,56 @@ namespace MarbleSorterGame
 {
     public class Gate : GameEntity
     {
-        public bool OpenStatus;
-        public bool Moving;
+        public bool IsFullyOpen => _gate.Position.Y <= _minGateY;
+        public bool IsFullyClosed => _gate.Position.Y >= _maxGateY;
+        
+        // Assuming 60 FPS: Tick = 16.6ms per frame
+        // Game ticks in 5 seconds = 5000 / 16.6 = 300 game ticks
+        private static int StepTicks = 100;
 
+        /// How much to in/decrement gate Y-position per-step
+        private float _step;
         private RectangleShape _gate;
-        private float _gate_open_max;
-        private float _gate_closed_max;
 
-        public Gate(Vector2f position, Vector2f size) : base(position,size)
+        private float _maxGateY;
+        private float _minGateY;
+
+        public void SetState(bool opening)
+        {
+            if (opening)
+                _step = - (Size.Y / StepTicks);
+            else
+                _step = (Size.Y / StepTicks);
+        }
+
+        public Gate(Vector2f position, Vector2f size) : base(position, size)
         {
             _gate = new RectangleShape(size);
             _gate.FillColor = SFML.Graphics.Color.Black;
             _gate.Position = position;
-
-            _gate_open_max = position.Y - size.Y;
-            _gate_closed_max = position.Y;
-
-            Moving = false;
+            
+            // Assuming 60 FPS: Tick = 16.6ms per frame
+            // Game ticks in 5 seconds = 5000 / 16.6 = 300 game ticks
+            _step = size.Y / StepTicks;
+            _minGateY = position.Y - size.Y;
+            _maxGateY = position.Y;
         }
 
-        /// <summary>
-        /// Toggles gate switches indicates whether it is open (true) or closed (false)
-        /// </summary>
-        public void Toggle()
+        public void Update()
         {
-            OpenStatus = !OpenStatus;
-        }
-
-        public void Move(float moveChange)
-        {
-            _gate.Position = new Vector2f(_gate.Position.X, _gate.Position.Y + moveChange);
-        }
-
-        public void Open(float moveChange)
-        {
-            if (Moving) return;
-            Moving = true;
-
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 100;
-            timer.Enabled = true;
-            timer.Elapsed += (Object source, System.Timers.ElapsedEventArgs e) =>
-            {
-                if (_gate.Position.Y <= _gate_open_max)
-                {
-                    timer.Stop();
-                    timer.Dispose();
-
-                    OpenStatus = true;
-                    Moving = false;
-                } else
-                {
-                    Move(-moveChange);
-                }
-            };
-        }
-
-        public void Close(float moveChange)
-        {
-            if (Moving) return;
-            Moving = true;
-
-            System.Timers.Timer timer = new System.Timers.Timer();
-            timer.Interval = 100;
-            timer.Enabled = true;
-            timer.Elapsed += (Object source, System.Timers.ElapsedEventArgs e) =>
-            {
-                if (_gate.Position.Y >= _gate_closed_max)
-                {
-                    timer.Stop();
-                    timer.Dispose();
-
-                    OpenStatus = true;
-                    Moving = false;
-                } else
-                {
-                    Move(moveChange);
-                }
-            };
-
+            float newY = Math.Min(Math.Max(_minGateY, _gate.Position.Y + _step), _maxGateY);
+            _gate.Position = new Vector2f(_gate.Position.X, newY);
+            Position = _gate.Position;
         }
 
         public override void Render(RenderWindow window)
         {
+            //base.Render(window);
             window.Draw(_gate);
         }
 
         public override void Load(IAssetBundle bundle)
         {
         }
-
     }
-
 }
