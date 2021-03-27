f8ff9440560a0cdf73be9c5bfb40dc153412b37f
Jesus Perez <jesus.perez.dev@gmail.com>
2021-03-26 22:14:41 -0400

    Merge conflicts

--- src/MarbleSorterGame/MarbleSorterGame/GameLoop.cs
+++ src/MarbleSorterGame/MarbleSorterGame/GameLoop.cs
@@ -50,15 +50,14 @@ namespace MarbleSorterGame
             WINDOW_WIDTH = windowWidth;
             WINDOW_HEIGHT = windowHeight;
             WINDOW_RECT = new RectangleShape {Size = new Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT)};
-            WINDOW = new RenderWindow(new VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), windowTitle);
-
-            WindowClearColor = windowClearColor;
-
+            
             Styles styles = Styles.Close;
             ContextSettings settings = default;
             settings.AntialiasingLevel = 8;
             
-            Window = new RenderWindow(new VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), windowTitle, styles, settings);
+            WINDOW = new RenderWindow(new VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), windowTitle, styles, settings);
+
+            WindowClearColor = windowClearColor;
             
             WINDOW.Closed += WINDOW_Closed;
             WINDOW.Resized += WINDOW_Resized;
