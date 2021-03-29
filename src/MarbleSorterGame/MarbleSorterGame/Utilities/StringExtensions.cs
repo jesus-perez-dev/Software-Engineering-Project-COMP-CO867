79c139ab927f6aad3b100e26658bc0e67c665716
jnlon <jnlon@users.noreply.github.com>
2021-03-28 21:46:49 -0400

    Add basic column wrap StringExtension

---
+++ src/MarbleSorterGame/MarbleSorterGame/Utilities/StringExtensions.cs
@@ -0,0 +1,26 @@
+using System.Text;
+
+namespace MarbleSorterGame.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string ColumnWrap(this string s, int maxColumn)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0, col = 0; i < s.Length; i++, col++)
+            {
+                if (col >= maxColumn)
+                {
+                    sb.Append("\n\t");
+                    col = 0;
+                }
+                
+                if (s[i] == '\n')
+                    col = 0;
+                
+                sb.Append(s[i]);
+            }
+            return sb.ToString();
+        }
+    }
+}
