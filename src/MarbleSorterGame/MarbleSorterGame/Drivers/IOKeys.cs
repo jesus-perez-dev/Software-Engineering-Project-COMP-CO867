f94fa304c789faa6e080186e97e8108cc7fe76f4
Stephen Kim <000801165@mohawkcollege.ca>
2021-03-30 03:19:15 -0400

    fixed legend layout and game win/lose popup layout

---
+++ src/MarbleSorterGame/MarbleSorterGame/Drivers/IOKeys.cs
@@ -0,0 +1,33 @@
+﻿namespace MarbleSorterGame.Drivers
+{
+    public class IOKeys
+    {
+        // Defines output keys of game entities
+        public static class Q
+        {
+            public const string TrapDoor1 = "TrapDoor1";
+            public const string TrapDoor2 = "TrapDoor2";
+            public const string TrapDoor3 = "TrapDoor3";
+            public const string Gate = "Gate";
+            public const string Conveyor = "Conveyor";
+        }
+
+        // Defines input keys of game entities
+        public static class I
+        {
+            public const string TrapDoor1Open = "TrapDoor1Open";
+            public const string TrapDoor2Open = "TrapDoor2Open";
+            public const string TrapDoor3Open = "TrapDoor3Open";
+            public const string TrapDoor1Closed = "TrapDoor1Closed";
+            public const string TrapDoor2Closed = "TrapDoor2Closed";
+            public const string TrapDoor3Closed = "TrapDoor3Closed";
+            public const string BucketMotionSensor = "BucketMotionSensor";
+            public const string GateOpen = "GateOpen";
+            public const string GateClosed = "GateClosed";
+            public const string ConveyorMotionSensor = "ConveyorMotionSensor";
+            public const string WeightSensor = "WeightSensor";
+            public const string ColorSensor = "ColorSensor";
+        }
+
+    }
+}
