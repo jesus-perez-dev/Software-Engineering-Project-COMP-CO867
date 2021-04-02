8a08a9dae0d45422f95ea8d6fac834d4d62d21fa
jnlon <jnlon@users.noreply.github.com>
2021-04-02 00:08:21 -0400

    Add GameState ToHumanString() and ToIndicatorColor() extension methods

--- src/MarbleSorterGame/MarbleSorterGame/Enums/GameState.cs
+++ src/MarbleSorterGame/MarbleSorterGame/Enums/GameState.cs
@@ -7,4 +7,29 @@ namespace MarbleSorterGame.Enums
         Progress,
         Pause
     }
+
+    public static class GameStateEnumExtensions 
+    {
+        public static string ToHumanString(this GameState self)
+        {
+            return self switch
+            {
+                GameState.Lose => "You Lost!",
+                GameState.Win => "You Won!",
+                GameState.Progress => "In-Progress",
+                GameState.Pause => "Paused",
+            };
+        }
+        
+        public static SFML.Graphics.Color ToIndicatorColor(this GameState self)
+        {
+            return self switch
+            {
+                GameState.Lose => SFML.Graphics.Color.Red,
+                GameState.Win => SFML.Graphics.Color.Green,
+                GameState.Progress => SFML.Graphics.Color.Yellow,
+                GameState.Pause => SFML.Graphics.Color.Cyan,
+            };
+        }
+    }
 }
